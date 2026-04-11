2995b4e30ac531dbdaed27e856d64161f30de27c
scoprire87 <acintolesi6@gmail.com>
2026-04-11 09:11:08 +0200

    Update __init__.py

--- custom_components/sureha/__init__.py
+++ custom_components/sureha/__init__.py
@@ -160,4 +160,104 @@ class SurePetcareAPI:
             LockState.LOCKED_ALL.name.lower(): self.surepy.sac.lock,
         }
 
-        await lock_states[state
+        await lock_states[state.lower()](flap_id)
+
+    async def async_setup(self) -> bool:
+        """Set up the Sure Petcare integration."""
+
+        _LOGGER.info("")
+        _LOGGER.info(
+            "%s %s", " \x1b[38;2;255;26;102m·\x1b[0m" * 24, choice(CATS)  # nosec
+        )
+        _LOGGER.info("  🐾   meeowww..! to the SureHA integration!")
+        # Riferimento aggiornato al tuo repository GitHub
+        _LOGGER.info("  🐾     code & issues: https://github.com/scoprire87/sureha")
+        _LOGGER.info(" \x1b[38;2;255;26;102m·\x1b[0m" * 30)
+        _LOGGER.info("")
+
+        await self.hass.config_entries.async_forward_entry_setups(self.config_entry, PLATFORMS)
+
+        surepy_entities: list[SurepyEntity] = self.coordinator.data.values()
+
+        pet_ids = [
+            entity.id for entity in surepy_entities if entity.type == EntityType.PET
+        ]
+
+        pet_location_service_schema = vol.Schema(
+            {
+                vol.Required(ATTR_PET_ID): vol.Any(cv.positive_int, vol.In(pet_ids)),
+                vol.Required(ATTR_WHERE): vol.Any(
+                    cv.string,
+                    vol.In(
+                        [
+                            Location.INSIDE.name.title(),
+                            Location.OUTSIDE.name.title(),
+                        ]
+                    ),
+                ),
+            }
+        )
+
+        async def handle_set_pet_location(call: Any) -> None:
+            """Call when setting the lock state."""
+
+            try:
+                if (pet_id := int(call.data.get(ATTR_PET_ID))) and (
+                    where := str(call.data.get(ATTR_WHERE))
+                ):
+                    await self.set_pet_location(pet_id, Location[where.upper()])
+                    await self.coordinator.async_request_refresh()
+
+            except ValueError as error:
+                _LOGGER.error(
+                    "🐾 \x1b[38;2;255;26;102m·\x1b[0m arguments of wrong type: %s", error
+                )
+
+        self.hass.services.async_register(
+            DOMAIN,
+            SERVICE_PET_LOCATION,
+            handle_set_pet_location,
+            schema=pet_location_service_schema,
+        )
+
+        async def handle_set_lock_state(call: Any) -> None:
+            """Call when setting the lock state."""
+
+            flap_id = call.data.get(ATTR_FLAP_ID)
+            lock_state = call.data.get(ATTR_LOCK_STATE)
+
+            await self.set_lock_state(flap_id, lock_state)
+            await self.coordinator.async_request_refresh()
+
+        flap_ids = [
+            entity.id
+            for entity in surepy_entities
+            if entity.type in [EntityType.CAT_FLAP, EntityType.PET_FLAP]
+        ]
+
+        lock_state_service_schema = vol.Schema(
+            {
+                vol.Required(ATTR_FLAP_ID): vol.All(cv.positive_int, vol.In(flap_ids)),
+                vol.Required(ATTR_LOCK_STATE): vol.All(
+                    cv.string,
+                    vol.Lower,
+                    vol.In(
+                        [
+                            LockState.UNLOCKED.name.lower(),
+                            LockState.LOCKED_IN.name.lower(),
+                            LockState.LOCKED_OUT.name.lower(),
+                            LockState.LOCKED_ALL.name.lower(),
+                        ]
+                    ),
+                ),
+            }
+        )
+
+        self.hass.services.async_register(
+            DOMAIN,
+            SERVICE_SET_LOCK_STATE,
+            handle_set_lock_state,
+            schema=lock_state_service_schema,
+        )
+
+        return True
